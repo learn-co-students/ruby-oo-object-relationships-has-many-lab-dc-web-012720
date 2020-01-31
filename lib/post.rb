f9890d1c7baf4f27f55673a39d7949fd625c7f97
bumbot <pkim1295@gmail.com>
2020-01-30 22:51:24 -0500

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,23 @@
+class Post
+    attr_reader :title
+    attr_accessor :author
+
+    @@all = []
+    def initialize(title)
+        @title = title
+
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def author_name
+        if self.author
+            return self.author.name
+        else
+            return nil
+        end
+    end
+end
